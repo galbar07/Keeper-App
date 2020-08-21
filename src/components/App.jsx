0de9bc96a31ee8510f498093d1996f5153681623
gal bar <galbar07@gmail.com>
2020-08-21 23:25:21 +0300

    Adding notes and map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function setNote(note){
+    return <Note
+    key = {note.id}
+    title = {note.title}
+    content = {note.content}
+    />
+}
+
 
 function App(){
 return <div>
     <Header/>
-    <Note/>
+    {notes.map(setNote)}
     <Footer/>
 </div>
- 
 }
 
 export default App;
